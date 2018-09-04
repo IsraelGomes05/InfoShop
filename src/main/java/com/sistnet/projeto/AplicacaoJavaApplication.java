201b35ef04be03c5c6beb48aeb9975ea3a6ce4a7
IsraelGomes05 <israelgomes05@gmail.com>
2018-09-04 00:53:32 -0400

    Projeto Criado

---
+++ src/main/java/com/sistnet/projeto/AplicacaoJavaApplication.java
@@ -0,0 +1,12 @@
+package com.sistnet.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoJavaApplication.class, args);
+	}
+}
