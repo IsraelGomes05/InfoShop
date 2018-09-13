fba04d00fd5c0d76656d33b2a18f0c82f4ced290
IsraelGomes05 <israelgomes05@gmail.com>
2018-09-12 23:53:13 -0400

    Paginação com parametros opcionais

--- src/main/java/com/sistnet/projeto/resources/CategoriaResource.java
+++ src/main/java/com/sistnet/projeto/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.sistnet.projeto.resources;
 
 import com.sistnet.projeto.dto.CategoriaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -53,6 +54,16 @@ public class CategoriaResource {
         List<Categoria> lista = service.findAll();
 
         List<CategoriaDTO> listaDto = lista.stream().map(CategoriaDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listaDto);
+    }
+
+    @RequestMapping(value = "/page",method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                       @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+                                                       @RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
+                                                       @RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+        Page<Categoria> lista = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<CategoriaDTO> listaDto = lista.map(CategoriaDTO::new);
         return ResponseEntity.ok().body(listaDto);
     }
 }
--- src/main/java/com/sistnet/projeto/services/CategoriaService.java
+++ src/main/java/com/sistnet/projeto/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import com.sistnet.projeto.services.exeptions.DataIntegrityExeption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.sistnet.projeto.domain.Categoria;
@@ -14,36 +17,41 @@ import com.sistnet.projeto.services.exeptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
-	@Autowired
-	private CategoriaRepository repo;
-	
-	public Categoria find(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
-	}
 
-	public Categoria insert(Categoria obj){
-		obj.setId(null);
-		return repo.save(obj);
-	}
+    @Autowired
+    private CategoriaRepository repo;
 
-	public Categoria update(Categoria obj) {
-		this.find(obj.getId());
-		return repo.save(obj);
-	}
+    public Categoria find(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
 
-	public void delete(Integer id) {
-		this.find(id);
-		try {
-			repo.deleteById(id);
-		} catch (DataIntegrityViolationException ex){
-			throw new DataIntegrityExeption("Não é possível excluir uma categoria que possui produtos");
-		}
-	}
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
 
-	public List<Categoria> findAll() {
-		return repo.findAll();
-	}
+    public Categoria update(Categoria obj) {
+        this.find(obj.getId());
+        return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        this.find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException ex) {
+            throw new DataIntegrityExeption("Não é possível excluir uma categoria que possui produtos");
+        }
+    }
+
+    public List<Categoria> findAll() {
+        return repo.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repo.findAll(pageRequest);
+    }
 }
