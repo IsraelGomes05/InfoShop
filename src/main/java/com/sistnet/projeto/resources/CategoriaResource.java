4a0fa98bfc3459dd7004537ccea7e119bd9ca8db
IsraelGomes05 <israelgomes05@gmail.com>
2018-09-13 00:36:30 -0400

    Validação sintática com Bean Validation

--- src/main/java/com/sistnet/projeto/resources/CategoriaResource.java
+++ src/main/java/com/sistnet/projeto/resources/CategoriaResource.java
@@ -11,6 +11,7 @@ import com.sistnet.projeto.services.CategoriaService;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.Servlet;
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,7 +30,8 @@ public class CategoriaResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+    public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto) {
+        Categoria obj = service.fromDTO(objDto);
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
@@ -37,7 +39,8 @@ public class CategoriaResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+    public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
+        Categoria obj = service.fromDTO(objDto);
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
@@ -57,11 +60,11 @@ public class CategoriaResource {
         return ResponseEntity.ok().body(listaDto);
     }
 
-    @RequestMapping(value = "/page",method = RequestMethod.GET)
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                                       @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
-                                                       @RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
-                                                       @RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+                                                       @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                       @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+                                                       @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
         Page<Categoria> lista = service.findPage(page, linesPerPage, orderBy, direction);
         Page<CategoriaDTO> listaDto = lista.map(CategoriaDTO::new);
         return ResponseEntity.ok().body(listaDto);
--- src/main/java/com/sistnet/projeto/services/CategoriaService.java
+++ src/main/java/com/sistnet/projeto/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.sistnet.projeto.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.sistnet.projeto.dto.CategoriaDTO;
 import com.sistnet.projeto.services.exeptions.DataIntegrityExeption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -53,5 +54,9 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
         return repo.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO(CategoriaDTO categoriaDTO){
+        return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
     }
 }
