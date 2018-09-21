12f0e227b00bbcf83e45c4cfe7cb81cffecde4a3
IsraelGomes05 <israelgomes05@gmail.com>
2018-09-20 23:59:16 -0400

    Validação customizada email não repetido insert Cliente

--- src/main/java/com/sistnet/projeto/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/sistnet/projeto/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.sistnet.projeto.services.validation;
 
+import com.sistnet.projeto.domain.Cliente;
 import com.sistnet.projeto.domain.enums.TipoCliente;
 import com.sistnet.projeto.dto.ClienteNewDTO;
+import com.sistnet.projeto.repository.ClienteRepository;
 import com.sistnet.projeto.resources.exeptions.FildMessage;
 import com.sistnet.projeto.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
 
     public void initialize(ClienteInsert constraint) {
     }
@@ -28,6 +34,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FildMessage("cpfOuCnpj", "CNPJ inválido"));
 
+        }
+
+        Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+        if (aux != null){
+            list.add(new FildMessage("Email", "Email já existe"));
         }
 
         // inclua os testes aqui, inserindo erros na lista
