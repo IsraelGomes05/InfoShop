3624d2462e074a7103a82e7b579c4d76ad70a7aa
IsraelGomes05 <israelgomes05@gmail.com>
2018-09-23 23:45:43 -0400

    Email HTML

--- src/main/java/com/sistnet/projeto/services/AbstractEmailService.java
+++ src/main/java/com/sistnet/projeto/services/AbstractEmailService.java
@@ -1,12 +1,25 @@
 package com.sistnet.projeto.services;
 
 import com.sistnet.projeto.domain.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.Date;
 
 public abstract class AbstractEmailService implements EmailService {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+    @Autowired
+    private JavaMailSender javaMailSender;
+
 
     @Value("${default.sender}")
     private String sender;
@@ -27,4 +40,33 @@ public abstract class AbstractEmailService implements EmailService {
         return sm;
     }
 
+    protected String htmlFromTemplatePedido(Pedido obj){
+        Context context = new Context();
+        context.setVariable("pedido", obj);
+        return templateEngine.process("email/confirmacaoPedido", context);
+    }
+
+    @Override
+    public void sendOrderConfirmationHtmlEmail(Pedido obj){
+        MimeMessage sm = null;
+        try {
+            sm = prepareMimeMessageFromPedido(obj);
+        } catch (MessagingException e) {
+            sendOrderConfirmationEmail(obj);
+        }
+        sendHtmlEmail(sm);
+    }
+
+    protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true);
+        messageHelper.setTo(obj.getCliente().getEmail());
+        messageHelper.setFrom(sender);
+        messageHelper.setSubject("Pedido confirmado! Código: " + obj.getId());
+        messageHelper.setSentDate(new Date(System.currentTimeMillis()));
+        messageHelper.setText(this.htmlFromTemplatePedido(obj),true);
+
+        return mimeMessage;
+    }
 }
--- src/main/java/com/sistnet/projeto/services/EmailService.java
+++ src/main/java/com/sistnet/projeto/services/EmailService.java
@@ -3,9 +3,15 @@ package com.sistnet.projeto.services;
 import com.sistnet.projeto.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage obj);
+
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
